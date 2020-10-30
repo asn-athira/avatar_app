c842c264a888aae6d70a07c3b43cf9a6cd857d3e
asn-athira <athira@rightsolution.ae>
2020-10-30 17:43:03 +0530

    modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 	protected
 
 	def configure_permitted_parameters
+		added_attrs=%i[username email password password_confirmation remember_me]
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
 		devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
 		
